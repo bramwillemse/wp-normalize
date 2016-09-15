dca90ac6f241eda2c45d5de5f87f8e4753c3781f
Bram Willemse <contact@bramwillemse.nl>
2016-09-15 13:13:22 +0200

    updated docs

--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 The idea of this plugin is to be a kind of CSS reset / normalize for WordPress.
 It strips some parts I never use or don't want my users to see/use and adds some things, like my phone number on the bottom of every dashboard page and extends user rights for the editor role.
 
-#TODO
+# Todo
 - Remove legacy code / unused modules
 - Create a roadmap
 - Split into modules
--- wp-normalize.php
+++ wp-normalize.php
@@ -1,11 +1,11 @@
 <?php /*
-	Plugin Name: Chram Base
-	Plugin URI: http://www.bramwillemse.nl
+	Plugin Name: WP Normalize
+	Plugin URI: https://github.com/bramwillemse/wp-normalize
 	Description: Set of base functions
 	Version: 1.0.2
 	Author: Bram Willemse
 	Author URI: http://www.bramwillemse.nl
-	Tested up to: 4.0
+	Tested up to: 4.6.1
 */
 
 /* 	==============================================================
